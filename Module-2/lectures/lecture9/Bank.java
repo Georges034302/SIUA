b7292f66aafb20208cbc1541ed85882ee26df49a
Georges034302 <georges.boughantous@gmail.com>
2022-09-21 15:49:06 +1000

    lec9

---
+++ Module-2/lectures/lecture9/Bank.java
@@ -0,0 +1,62 @@
+
+public class Bank{
+    //Fields
+    private Customer rashed;
+    private Customer aiman;
+    private Customer yousif;
+    private Customer customer;
+    
+    public Bank(){
+        rashed = new Customer("Rashed",2000);        
+        aiman = new Customer("Aiman",1000);
+        yousif = new Customer("Yousif",5000);
+        customer = new Customer();
+    }
+    
+    private void deposit(){
+        rashed.deposit(2000);
+        aiman.deposit(500);
+        yousif.deposit(1000);
+    }
+   
+    private void show(){
+        System.out.println(rashed.getName());
+        System.out.println(aiman.getName());
+        System.out.println(yousif);
+        System.out.println(customer);
+    }
+    
+    private void updateNames(){
+        rashed.setName("Elvis");
+        aiman.setName("Carla");
+        yousif.setName("James");
+    }
+    
+    private void update(){
+        rashed.update(rashed.readName(), rashed.readBalance());
+        aiman.update(aiman.readName(), aiman.readBalance());
+        yousif.update(yousif.readName(), yousif.readBalance());
+    }
+    
+    private char readChoice(){
+        System.out.print("Choice(d/s/u/x): ");
+        return In.nextChar();
+    }
+    
+    private void menu(){
+        char c;
+        
+        while((c = readChoice()) != 'x'){
+            switch(c){
+                case 'd': deposit();break;
+                case 's': show(); break;
+                case 'u': update(); break;
+                default: System.out.println("Unknown Choice");
+            }            
+        }
+    }
+    
+    public static void main(String[] args){
+        new Bank().menu();
+    }
+}
---
+++ Module-2/lectures/lecture9/Person.java
@@ -0,0 +1,50 @@
+
+public class Person{
+    //Fields
+    private String name;
+    private int age;
+    
+    //Default Constructor
+    public Person(){}
+    
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    
+    public boolean match(String name){
+        return this.name.equals(name);
+    }
+    
+    //Accessor
+    public String getName(){
+        return this.name;
+    }
+    
+    public int getAge(){
+        return this.age;
+    }
+    
+    //Mutators
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public void setAge(int age){
+        this.age = age;
+    }
+    
+    @Override
+    public String toString(){
+        return this.name+" age is "+this.age;
+    }
+}
+
+
+
+
+
+
+
+
+
