bf63fb6988c957e1bec589ad55a98ea074a8443d
Georges034302 <georges.boughantous@gmail.com>
2022-09-30 14:42:06 +1000

    lab11-3

---
+++ Module-2/labs/lab11/Bank.java
@@ -0,0 +1,102 @@
+import java.util.*;
+
+public class Bank{
+    private List<Customer> customers = new ArrayList();
+
+    public Bank(){        
+    }
+
+    private void register(){
+        for(int i=0; i < 3; i++)
+            this.customers.add(new Customer());
+    }
+
+    private Customer customer(String name){
+        for(Customer c:customers)
+            if(c.match(name))
+                return c;
+        return null;
+    }
+
+    private void addUnique(){
+        String name = In.readString("Name: ");
+        Customer c = customer(name);
+
+        if(c != null)
+            System.out.println("Customer exists");
+        else{
+            Customer customer = new Customer(name);
+            this.customers.add(customer);
+        }
+    }
+
+    private List<Customer> matches(String name){        
+        List<Customer> temp = new ArrayList();
+        for(Customer c:customers)
+            if(c.match(name))
+                temp.add(c);
+        return temp;
+    }
+
+    private void delete(){
+        String name = In.readString("Name: ");
+        List<Customer> temp = matches(name);
+
+        if(temp.size() > 0)
+            this.customers.removeAll(temp);
+        else
+            System.out.println("Customer does not exist");
+    }
+
+    private void show(){
+        for(Customer c:customers)
+            System.out.println(c);
+    }
+
+    private char readChoice(){
+        System.out.print("Bank Menu(c/r/a/d/s/x): ");
+        return In.nextChar();
+    }
+
+    private void customerMenu(){
+        String name = In.readString("Name: ");
+        Customer c = customer(name);
+        if(c != null)
+            c.menu();
+        else
+            System.out.println("Customer "+name+" does not exist");
+    }
+
+    private void menu(){
+        char c;
+
+        while((c = readChoice()) != 'x'){
+            switch(c){
+                case 'c':customerMenu();break;
+                case 'r': register();break;
+                case 'a':addUnique();break;
+                case 'd':delete(); break;
+                case 's':show();break;
+                default: help();break;
+            }
+        }
+    }
+
+    private void help(){
+        System.out.println("c - go to customer menu");
+        System.out.println("r - Register customers");
+        System.out.println("a - add account");
+        System.out.println("d - delete");    
+        System.out.println("s - show all");
+        System.out.println("x - exit");
+    }
+    
+    public static void main(String[] args){
+        (new Bank()).menu();
+    }
+}
+
+
+
+
+
---
+++ Module-2/labs/lab11/Customer.java
@@ -0,0 +1,108 @@
+import java.util.*;
+
+public class Customer{
+    private String name;
+    private List<Account> accounts = new ArrayList();
+
+    public Customer(){
+        this.name = In.readString("Name: ");
+    }
+
+    public Customer(String name){
+        this.name = name;
+    }
+    
+    // private String readName(){
+    // System.out.print("Name: ");
+    // return In.nextLine();
+    // }
+
+    public boolean match(String name){
+        return this.name.equals(name.strip());
+    }
+
+    //only add an account if the type does not exist
+    private void addUnique(String type){
+        Account a = account(type);
+        if(a != null)
+            System.out.println(type+" already exists");
+        else{
+            Account account = new Account(type);
+            this.accounts.add(account);
+        }
+    }
+
+    private void add(){
+        String type = In.readString("Type: ");
+        addUnique(type);
+    }
+    
+    //look up function that uses the any pattern
+    private Account account(String type){
+        for(Account a:accounts)
+            if(a.match(type))
+                return a;
+        return null;
+    }
+
+    // private String readType(){
+    // System.out.print("Type: ");
+    // return In.nextLine();
+    // }
+
+    //updated look up pattern to find all matches
+    private List<Account> matches(String type ){
+        List<Account> temp = new ArrayList();
+
+        for(Account a:accounts)
+            if(a.match(type))
+                temp.add(a);
+        return temp;
+    }
+
+    private void remove(){
+        String type = In.readString("Type: ");
+        List<Account> temp = matches(type);
+        accounts.removeAll(temp);
+    }
+
+    private void show(){
+        for(Account a:accounts)
+            System.out.println(a);
+    }
+
+    private char readChoice(){
+        System.out.print("Customer Menu(A/D/S/X): ");
+        return In.nextChar();
+    }
+
+    public void menu(){
+        char c;
+        while((c = readChoice()) != 'X'){
+            switch(c){
+                case 'A':add();break;
+                case 'D':remove();break;
+                case 'S':show();break;
+                default: help();break;
+            }
+        }
+    }
+    
+    private  void help(){
+        System.out.println("A - Add Account");
+        System.out.println("D - Delete Account");
+        System.out.println("S - Show Accounts");
+        System.out.println("X - exit");
+    }
+
+    @Override
+    public String toString(){
+        return this.name+" has "+this.accounts;
+    }
+}
+
+
+
+
+
+
