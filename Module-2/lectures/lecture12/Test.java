3c218550452b2b4f27513afafcb52d85f571ece9
Georges034302 <georges.boughantous@gmail.com>
2022-10-05 15:24:24 +1100

    lec12b

---
+++ Module-2/lectures/lecture12/Test.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Test{
+    private List<List<User>> users = new ArrayList();
+
+    public Test(){}
+
+    private List<List<User>> populate(){
+        int rows = readInt("Rows = ");
+        List<List<User>> outer = new ArrayList(rows);
+        
+        for(int i=0; i< rows; i++){
+            int cols = readInt("Cols = ");
+            List<User> inner = new ArrayList(cols);           
+            for(int j = 0 ; j< cols; j++)
+                inner.add(j,new User(readName()));
+            outer.add(i, inner);
+        }
+        return outer;
+    }
+
+    private String readName(){
+        System.out.print("Name = ");
+        return In.nextLine();
+    }
+
+    private int readInt(String prompt){
+        System.out.print(prompt);
+        return In.nextInt();
+    }
+}
